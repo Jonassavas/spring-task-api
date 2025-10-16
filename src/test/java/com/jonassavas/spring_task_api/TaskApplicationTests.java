72403d115c26504ebf300e53906f2a369f089f13
Jonas Sävås <jonas.savas123@gmail.com>
2025-10-17 01:06:16 +0200

    Initial commit

---
+++ src/test/java/com/jonassavas/spring_task_api/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonassavas.spring_task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

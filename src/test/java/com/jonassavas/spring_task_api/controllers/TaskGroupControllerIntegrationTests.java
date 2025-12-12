0ec7a7da245d7b09a767cc9305d021011b6f79ce
Jonas Sävås <jonas.savas123@gmail.com>
2025-12-12 23:48:30 +0100

    Adds an outline test class for the test group controller

---
+++ src/test/java/com/jonassavas/spring_task_api/controllers/TaskGroupControllerIntegrationTests.java
@@ -0,0 +1,12 @@
+package com.jonassavas.spring_task_api.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+public class TaskGroupControllerIntegrationTests {
+    
+}

3141f88d44040d15df103c75e2af7cdb6f85d7b8
Jonas Sävås <jonas.savas123@gmail.com>
2026-01-05 23:54:05 +0100

    Adds the outline for the task group controller integration tests

--- src/test/java/com/jonassavas/spring_task_api/controllers/TaskGroupControllerIntegrationTests.java
+++ src/test/java/com/jonassavas/spring_task_api/controllers/TaskGroupControllerIntegrationTests.java
@@ -1,12 +1,34 @@
 package com.jonassavas.spring_task_api.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jonassavas.spring_task_api.services.TaskGroupService;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 public class TaskGroupControllerIntegrationTests {
-    
+   
+    private TaskGroupService taskGroupService;
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public TaskGroupControllerIntegrationTests(MockMvc mockMvc, TaskGroupService taskGroupService, ObjectMapper objectMapper){
+        this.mockMvc = mockMvc;
+        this.taskGroupService = taskGroupService;
+        this.objectMapper = objectMapper;
+    }
+
+    @Test
+    public void testThatCreateTaskGroupReturnsHttp201Create() throws Exception{
+
+    }
 }

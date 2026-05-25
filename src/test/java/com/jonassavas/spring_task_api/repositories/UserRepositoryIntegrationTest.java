6edc9a027c994dc54c2e52d36c5d4813b849f432
Jonas Sävås <jonas.savas123@gmail.com>
2026-05-25 23:15:07 +0200

    Formats the pom.xml and runs spotless again

--- src/test/java/com/jonassavas/spring_task_api/repositories/UserRepositoryIntegrationTest.java
+++ src/test/java/com/jonassavas/spring_task_api/repositories/UserRepositoryIntegrationTest.java
@@ -3,13 +3,11 @@ package com.jonassavas.spring_task_api.repositories;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.jonassavas.spring_task_api.domain.entities.UserEntity;
+import com.jonassavas.util.TestUserData;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import com.jonassavas.spring_task_api.domain.entities.UserEntity;
-import com.jonassavas.util.TestUserData;
-
 
 @DataJpaTest
 public class UserRepositoryIntegrationTest {
